34ad1fa680e3773ecd4663320a363220422ab9de
Priyankateke <priiyankateke1705@gmail.com>
2020-04-11 06:13:41 -0700

    ADDED: POJO And Main Class To Parse CSV Into POJO

---
+++ src/main/java/POJO/CSVUser.java
@@ -0,0 +1,35 @@
+package POJO;
+
+import com.opencsv.bean.CsvBindByName;
+
+public class CSVUser {
+    @CsvBindByName
+    private String name;
+
+    @CsvBindByName(column = "email", required = true)
+    private String email;
+
+    @CsvBindByName(column = "phone")
+    private String phoneNo;
+
+    @CsvBindByName
+    private String country;
+
+    // Getters and Setters (Omitted for brevity)
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+}
